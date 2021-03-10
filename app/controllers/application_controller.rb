906cfee69b892ed376b8bf411784c6021b28fa18
MayaraFernandes <mayarafernandesetec@gmail.com>
2021-03-10 15:26:45 -0300

    create ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,15 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(_resource)
     if current_user.role == 'guide'
-      guides_path
+      new_guide_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_up_path_for(_resource)
+    if current_user.role == 'guide'
+      new_guide_path
     else
       root_path
     end
